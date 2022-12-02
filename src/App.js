8612b962e81afe65bbac5b6f65deede5d549ba17
Students <qazaqbaeva>
2022-12-02 15:07:58 +0600

    endterm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import CharacterTable from './components/CharacterTable'
+import axios from 'axios'
+import React , {useEffect,useState} from 'react'
+import Search from './components/Search'
+
+const hash =  "268fcd8a06b54336a66950f347002f46"
 
 function App() {
+  const[items,setItems] = useState([])
+  const[isLoading,setLoading] = useState(true)
+  const [query,setQuery] = useState('')
+
+  useEffect(()=>{
+    const fetch = async()=>{
+      if(query===''){
+          const result = await axios(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=711b887f108243210d9c1802511dd36a&hash=${hash}`)
+          console.log(result.data.data.results)
+          setItems(result.data.data.results)
+          setLoading(false)
+        }
+      else{
+        const result = await axios(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&ts=1&apikey=711b887f108243210d9c1802511dd36a&hash=${hash}`)
+        console.log(result.data.data.results)
+        setItems(result.data.data.results)
+        setLoading(false)
+      }
+
+    }
+
+    fetch()
+  },[query])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <Header/>
+        <Search search={(q)=>setQuery(q)}></Search>
+        <CharacterTable items={items} isLoading={isLoading} />
+      </div>
   );
 }
 
---
+++ src/components/CharacterItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const CharacterItem = ({item}) => {
+
+
+    return (
+        <div className='content'>
+      <div className='content-inner'>
+        <div className='content-front'>
+          <img src={item.thumbnail.path + "/portrait_xlarge.jpg"} alt='' />
+        </div>
+        <div className='content-back'>
+          <h1>{item.name}</h1>
+          <ul>
+            <li>
+              <strong>Name:</strong> {item.name}
+            </li>
+            <li>
+              <strong>Description:</strong> {item.description}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default CharacterItem
